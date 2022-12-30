8dcbc2cf129a21d7141db7db285d410b951c546f
Richard Stiehm <richardstiehm@gmail.com>
2022-12-30 10:15:53 -0500

    Create Body Section

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,13 @@ import User from "./User"
 const Header = () => {
   return (
     <header className="flex justify-between p-5 text-sm text-gray-700">
-      <div className="flex space-x-4 item-center">
-        <p className="link">About</p>
-        <p className="link">Store</p>
+      <div className="flex space-x-4 item-center justify-center">
+        <p className="link my-2">About</p>
+        <p className="link my-2">Store</p>
       </div>
       <div className="flex space-x-4 item-center">
-        <p className="link">Gmail</p>
-        <p className="link">Images</p>
+        <p className="link my-2">Gmail</p>
+        <p className="link my-2">Images</p>
         <User />
       </div>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
+import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid"
+
 export default function Home() {
   return (
     <div>
@@ -14,7 +15,19 @@ export default function Home() {
       <Header />
 
       {/* BODY */}
+      <form className='flex flex-col items-center mt-40'>
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png" width={300} height={100} objectFit="cover"/>
+        <div className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-3xl'>
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <input type="text" className='flex-grow focus:outline-none'/>
+          <MicrophoneIcon className='h-5 text-gray-500' />
+        </div>
+        <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
 
+      </form>
 
       {/* FOOTER */}
 
