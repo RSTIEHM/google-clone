16b4a2f4e93a2840b88cac795d7d7826fe685ebe
Richard Stiehm <buzzysanders@gmail.com>
2022-12-31 15:41:34 -0500

    BASIC API SEARCH WORKING

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import SearchHeader from '../components/SearchHeader'
-const Search = () => {
+
+
+const Search = ({results}) => {
+  console.log(results)
   return (
     <div>
         <Head>
@@ -15,3 +18,12 @@ const Search = () => {
 }
 
 export default Search
+
+export async function getServerSideProps(ctx) {
+  const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${ctx.query.term}${ctx.query.searchType && "&searchType=image"}`).then((res) => res.json())
+  return {
+    props: {
+      results: data
+    }
+  }
+}
